6fffa872a3c15964d681cd383032b5cc01cdb81b
Pablo Giraldo <pablogirald@yahoo.es>
2020-10-15 23:46:25 +0200

    Test db conn

--- src/info/pablogiraldo/tienda_bd/dao/ProductDAO.java
+++ src/info/pablogiraldo/tienda_bd/dao/ProductDAO.java
@@ -45,26 +45,15 @@ public class ProductDAO {
 
 	public List<Product> listAllProducts() throws SQLException {
 
-		Statement statement = null;
-		ResultSet resultSet = null;
-
 		List<Product> listProducts = new ArrayList<>();
 		String sql = "SELECT * FROM products";
-		connection = getConnection();
 
 //		connection = DbConnection.getConnection();
 
 		connection = getConnection();
 
-		if (connection != null) {
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery(sql);
-		} else {
-			connection = DbConnection.getConnection();
-
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery(sql);
-		}
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery(sql);
 
 		while (resultSet.next()) {
 
