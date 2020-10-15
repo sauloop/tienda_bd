d4c831571b4360c40ca180ce7442d88b9f37994a
Pablo Giraldo <pablogirald@yahoo.es>
2020-10-16 00:06:06 +0200

    Add base controller

--- src/info/pablogiraldo/tienda_bd/controller/BaseController.java
+++ src/info/pablogiraldo/tienda_bd/controller/BaseController.java
@@ -1,43 +1,25 @@
 package info.pablogiraldo.tienda_bd.controller;
 
 import java.io.IOException;
-import java.sql.SQLException;
-
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import info.pablogiraldo.tienda_bd.dao.ProductDAO;
-import info.pablogiraldo.tienda_bd.model.Product;
-import info.pablogiraldo.tienda_bd.common.Data;
 
 /**
- * Servlet implementation class CarroController
+ * Servlet implementation class BaseController
  */
-@WebServlet("/carro")
-public class CarroController extends HttpServlet {
+@WebServlet("")
+public class BaseController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	private ProductDAO productDAO;
-
-	Product prod;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public CarroController() {
+	public BaseController() {
 		super();
 		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		productDAO = new ProductDAO();
-		prod = null;
 	}
 
 	/**
@@ -46,21 +28,7 @@ public class CarroController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		if (request.getParameter("id") != null && !request.getParameter("id").isEmpty()) {
-			int index = Integer.parseInt(request.getParameter("id"));
-
-			try {
-
-				prod = productDAO.getProduct(index);
-
-			} catch (SQLException ex) {
-				throw new ServletException(ex);
-			}
-
-			Data.carro.add(prod);
-
-			response.sendRedirect(request.getContextPath() + "home?mensaje=" + prod.getName() + " comprado.");
-		}
+		response.sendRedirect(request.getContextPath() + "home");
 	}
 
 	/**
