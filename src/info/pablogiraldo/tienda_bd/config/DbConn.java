110bc37296f4c1d49247dfa36f97bd5aa47ca85a
Pablo Giraldo <pablogirald@yahoo.es>
2020-10-18 15:24:37 +0200

    Add env properties

--- src/info/pablogiraldo/tienda_bd/config/DbConn.java
+++ src/info/pablogiraldo/tienda_bd/config/DbConn.java
@@ -3,14 +3,18 @@ package info.pablogiraldo.tienda_bd.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbConn {
 
+	public static Properties envProperties;
+
 	private static Connection conn = null;
+
 	private String driver;
 	private String url;
-	private String usuario;
-	private String password;
+	private String user;
+	private String pass;
 
 	private DbConn() {
 
@@ -24,9 +28,9 @@ public class DbConn {
 
 		// Prod
 
-		url = "jdbc:mysql://ryfqldzbliwmq6g5.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/vfyv7vwy5u7165ss";
-		usuario = "m9rbuvbdcexo6zye";
-		password = "r86jezkjgdc8mb7z";
+//		url = "jdbc:mysql://ryfqldzbliwmq6g5.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/vfyv7vwy5u7165ss";
+//		usuario = "m9rbuvbdcexo6zye";
+//		password = "r86jezkjgdc8mb7z";
 
 //		try {
 //			Class.forName(driver);
@@ -35,10 +39,14 @@ public class DbConn {
 //			e.printStackTrace();
 //		}
 
+		url = envProperties.getProperty("db.url");
+		user = envProperties.getProperty("db.user");
+		pass = envProperties.getProperty("db.pass");
+
 		try {
 			Class.forName(driver);
 
-			conn = DriverManager.getConnection(url, usuario, password);
+			conn = DriverManager.getConnection(url, user, pass);
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -56,4 +64,11 @@ public class DbConn {
 
 		return conn;
 	}
+
+	public static void resetConnection() {
+
+		new DbConn();
+
+	}
+
 }
